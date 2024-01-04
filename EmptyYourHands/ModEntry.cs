2516b3ab58a37d4b651dbc086622800a6269f35e
Afif Shafara <Afif Shafara@AS-PC>
2024-01-04 18:11:08 +0700

    Add project files.

---
+++ EmptyYourHands/ModEntry.cs
@@ -0,0 +1,64 @@
+﻿using GenericModConfigMenu;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace EmptyYourHands
+{
+    /// <summary>The mod entry point.</summary>
+    internal class ModEntry : Mod
+    {
+        /*********
+        ** Properties
+        *********/
+        /// <summary>The mod configuration from the player.</summary>
+        ModConfig Config;
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            Config = Helper.ReadConfig<ModConfig>();
+
+            helper.Events.GameLoop.GameLaunched += GameLaunched;
+            helper.Events.Input.ButtonPressed += ButtonPressed;
+        }
+
+        private void ButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady) return;
+
+            if (e.Button == Config.Keybind)
+            {
+                if (!Game1.player.IsBusyDoingSomething()) 
+                    Game1.player.CurrentToolIndex = int.MaxValue;
+            }
+        }
+
+        private void GameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null) return;
+
+            configMenu.Register(
+                mod: ModManifest,
+                reset: () => Config = new ModConfig(),
+                save: () => Helper.WriteConfig(Config));
+
+            configMenu.AddKeybind(
+                mod: ModManifest,
+                name: () => "Keybind",
+                getValue: () => Config.Keybind,
+                setValue: value => Config.Keybind = value);
+        }
+    }
+
+    class ModConfig
+    {
+        public SButton Keybind = SButton.X;
+    }
+}
